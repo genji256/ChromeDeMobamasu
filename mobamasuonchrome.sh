7656b9af263d2d6624b15dae692b711f0cda6c03
Elichika <elichika@Elichika-no-MacBook-Air.local>
2015-02-24 09:48:09 +0900

    first commit

---
+++ mobamasuonchrome.sh
@@ -0,0 +1,4 @@
+#!/bin/bash
+
+#ChromeをiPhone6 ios8に偽装して起動
+open -a Google\ Chrome --args --user-agent="Mozilla/5.0 (iPhone; CPU iPhone OS 8_0 like Mac OS X) AppleWebKit/600.1.3 (KHTML, like Gecko) Version/8.0 Mobile/12A4345d Safari/600.1.4"
